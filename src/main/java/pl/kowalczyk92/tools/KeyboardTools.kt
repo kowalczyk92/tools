7864a6ea11e89823ddfe7f5db13b2b7f0b099e4a
Łukasz Kowalczyk <lukasz.kowalczyk@xtb.com>
2022-02-24 11:46:52 +0100

    b

--- src/main/java/pl/kowalczyk92/tools/KeyboardTools.kt
+++ src/main/java/pl/kowalczyk92/tools/KeyboardTools.kt
@@ -16,3 +16,7 @@ fun Activity.showKeyboard(editText: EditText) {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
     imm?.showSoftInput(editText, 0)
 }
+
+
+
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
