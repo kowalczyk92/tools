35bccba417c276b660b654fc4a805f1243a00abc
Łukasz Kowalczyk <lukasz.kowalczyk92@gmail.com>
2023-08-06 15:18:28 +0200

    mvi fix

--- src/main/java/pl/kowalczyk92/tools/flow/MVIViewModel.kt
+++ src/main/java/pl/kowalczyk92/tools/flow/MVIViewModel.kt
@@ -1,11 +1,11 @@
 package pl.kowalczyk92.tools.flow
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 abstract class MVIViewModel<T : UIState, in E : UIEvent> : ViewModel() {
 
-    abstract val state: Flow<T>
+    abstract val state: StateFlow<T>
 
     protected abstract fun reduce(event: E)
 }
